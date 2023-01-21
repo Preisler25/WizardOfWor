1be24646076afae37e2d4dce56c2da8f0f544dfb
Preisler25 <preisler.andras@gmail.com>
2023-01-21 18:42:45 +0100

    main

--- backend/modules/main.js
+++ backend/modules/main.js
@@ -5,22 +5,19 @@ let genGame = () => {
     let map = new Map();
     let player = new char.Player("player", 0);
     map.player = player;
-    let Rotpint = new char.Rotpint("rotpints", [0, 1, 3], {x:0 , y:100});
-    map.rotpints.push(Rotpint);
-    let Rotpint1 = new char.Rotpint("rotpints", [3,1], {x:0 , y:272});
-    map.rotpints.push(Rotpint1);
-    let Rotpint2 = new char.Rotpint("rotpints", [1, 2], {x:590 , y:272});
-    map.rotpints.push(Rotpint2);
-    let Rotpint3 = new char.Rotpint("rotpints", [0, 2, 1], {x:590 , y:100});
-    map.rotpints.push(Rotpint3);
-    let Rotpint4 = new char.Rotpint("rotpints", [0, 2], {x:590 , y:0});
-    map.rotpints.push(Rotpint4);
-    let Rotpint5 = new char.Rotpint("rotpints", [0, 3], {x:0 , y:0});
-    map.rotpints.push(Rotpint5);
-    let enemy = new char.Enemy("base", 0);
-    map.enemies.push(enemy);
-    let enemy1 = new char.Enemy("base", 1);
-    map.enemies.push(enemy1);
+    map.rotpints.push(
+        new char.Rotpint("rotpints", [0, 1, 3], {x:0 , y:100}),
+        new char.Rotpint("rotpints", [3,1], {x:0 , y:272}),
+        new char.Rotpint("rotpints", [1, 2], {x:590 , y:272}),
+        new char.Rotpint("rotpints", [0, 2, 1], {x:590 , y:100}),
+        new char.Rotpint("rotpints", [0, 2], {x:590 , y:0}),
+        new char.Rotpint("rotpints", [0, 3], {x:0 , y:0})
+        );
+    map.enemies.push(
+        new char.Enemy("base", 0),
+        new char.Enemy("base", 1)
+
+    );
     return map;
 }
 
@@ -57,7 +54,7 @@ let moveEnemy = (map) => {
             case 3:
                 e.moveRight();
                 break;
-        }S
+        }
         e.test(map.rotpints)
     });
     checkCollision(map);
