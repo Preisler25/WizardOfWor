941258ef418ec655bd5ccc2722cba23166043cf2
Preisler <preisler.andras@gmail.com>
2023-05-06 18:36:18 +0200

    main

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-io.on('connection', (socket) => {
-    socket.on('message', (data) => {
-        socket.send(data);
-    });
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+
+  socket.on('move', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('move', msg);
+  });
 });
 
-http.listen(3000, () => {
-    console.log('Server is running on port 3000');
+http.listen(3000, function() {
+  console.log('listening on *:3000');
 });
