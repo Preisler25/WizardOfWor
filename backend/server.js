58278c09439b7797ec868c84990c6cbf4fa884a7
Preisler <preisler.andras@gmail.com>
2023-05-06 18:21:50 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -2,72 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const path = require('path');
-const bodyParser = require('body-parser');
-
-const sqlfucntions = require('./modules/sqlfunctions');
-
-const main = require('./modules/main');
-
-/*
-
-sqlfucntions.register(incom)
-incom = {username, password, password2, email}
-
-sqlfucntions.login(incom)
-incom = {username, password}
-
-*/
-
-
-
-app.set('viewengine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', (req, res) => {
-    res.render(path.join(__dirname, 'views', 'index.ejs'));
-});
-app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'css', 'main.css'));
-});
-
-app.get('/sources', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'sources', req.query.file));
-});
 
 io.on('connection', (socket) => {
-    let map = main.genGame();
     socket.on('message', (data) => {
-        main.movePlayer(data, map);
+        socket.send(data);
     });
-    let game = setInterval(() => {       
-        if (!map.ingame) {
-            socket.send('gameover');
-            console.log('gameover');
-            clearInterval(game);
-            clearInterval(send);
-            clearInterval(moveEnemys);
-            //saving data 
-            //sqlsave(map.points )
-        }
-        if (!socket.connected) {
-            console.log('disconnected');
-            clearInterval(game);
-            clearInterval(send);
-            clearInterval(moveEnemys);
-        }
-    }, 1000/144);
-    let moveEnemys = setInterval(() => {
-        main.moveEnemy(map);
-        main.moveBullet(map);
-        main.test(map)
-    }, 1000/144);
-    let send = setInterval(() => {
-        socket.send('map ' + JSON.stringify(map));
-    }, 1000/144);
 });
-
 
 http.listen(3000, () => {
     console.log('Server is running on port 3000');
